7678cb3758638ba754da993d646fb601eaa1bd6a
Ankit <mittalankit2404@gmail.com>
2022-08-07 16:31:34 +0530

    version 1.0.1

--- backend/server.js
+++ backend/server.js
@@ -33,19 +33,19 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.Node_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  //   app.use(express.static(path.join(__dirname, "/frontend/build")));
+  //   app.get("*", (req, res) =>
+  //     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  //   );
+  // } else {
+  //   app.get("/", (req, res) => {
+  //     res.send("api is working");
+  //   });
+  app.use(express.static("frontend/build"));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
-} else {
-  app.get("/", (req, res) => {
-    res.send("api is working");
-  });
-  // app.use(express.static("frontend/build"));
-
-  // app.get("*", (req, res) =>
-  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  // );
 }
 
 app.use(notFound);
