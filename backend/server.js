98a1283290a65f7b04079f9e2dfb9db76bb39368
Ankit <mittalankit2404@gmail.com>
2022-08-07 14:47:17 +0530

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ if (process.env.Node_ENV == "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("api is working");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -32,10 +28,25 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
 const __dirname = path.resolve();
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.Node_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "/frontend/build")));
+  //   app.get("*", (req, res) =>
+  //     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  //   );
+  // } else {
+  //   app.get("/", (req, res) => {
+  //     res.send("api is working");
+  //   });
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
 
 app.use(notFound);
 
