8a72083244358edef3d9ce0173a1f364e64d655b
Ankit <mittalankit2404@gmail.com>
2023-01-02 13:16:22 +0530

    deployment on cyclic

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const con = await mongoose.connect(process.env.MONGO_URI , {
-            useUnifiedTopology : true,
-            useNewUrlParser : true,
-        })
+  try {
+    const con = await mongoose.connect(process.env.MONGO_URI);
 
-        console.log(`Mongodb Connect: ${con.connection.host}`)
-    }catch(error){
-        console.error(error.message)
-        process.exit(1);
-    }
-}
+    console.log(`Mongodb Connect: ${con.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
